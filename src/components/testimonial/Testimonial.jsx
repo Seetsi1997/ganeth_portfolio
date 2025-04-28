42a2a93f9491797d457fad9288947350a1c91e47
Ganeth <seetsiganeth@gmail.com>
2025-04-28 02:37:59 +0200

    Update Testimonial.jsx

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -24,7 +24,7 @@ const Testimonial = () => {
   async function likeTestimonial(id) {
     try {
       // Send a like request to the API
-      await fetch(`${process.env.REACT_APP_API_URL}/testimonials/like/${id}`, { method: "POST" });
+      await fetch(`${process.env.REACT_APP_API_URL}/testimonials/likes/${id}`, { method: "POST" });
 
       // Reload testimonials after liking to get updated data (or you could just update the likes count locally)
       const response = await axios.get(`${process.env.REACT_APP_API_URL}/testimonials`);
